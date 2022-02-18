5bdfa36a0e770d2f6e504438853841187f01b537
TrpFrog <dev@trpfrog.net>
2022-02-19 06:52:28 +0900

    Create color_hisgram.m

---
+++ src/color_hisgram.m
@@ -0,0 +1,18 @@
+function res = color_hisgram(fig)
+%COLOR_HISGRAM 64色カラーヒストグラムを作る関数
+%   詳細説明をここに記述
+
+    % 64色に減色
+    RED = fig(:,:,1); GREEN = fig(:,:,2); BLUE = fig(:,:,3);
+    reduced = uint8(floor(double(RED)/64) * 4 * 4 ...
+        + floor(double(GREEN)/64) * 4 + floor(double(BLUE)/64));
+
+    % ヒストグラムを作成
+    res = zeros(1, 64);
+    n = size(reduced, 1) * size(reduced, 2);
+    for i = 1 : n
+        color = fig(i) + 1; % 1-indexed
+        res(color) = res(color) + 1;
+    end
+end
+
