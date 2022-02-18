f075d8109a7b4661102dc1db587ff79cb17667d2
TrpFrog <dev@trpfrog.net>
2022-02-19 06:52:49 +0900

    Create good_resize.asv

---
+++ src/good_resize.m
@@ -0,0 +1,14 @@
+function img = good_resize(img)
+%GOOD_RESIZE いい感じにリサイズする関数
+%   画素数がなるべく 320 * 240 に近くなるようにリサイズする
+
+    [H, W, ~] = size(img);
+    disp(H)
+    disp(W)
+
+    goal = 320 * 240;
+    h = round(sqrt(H / W * goal));
+    w = round(sqrt(W / H * goal));
+    
+    img = imresize(img, [h, w]);
+end
