a2c51078f7c13e8dfb0e4145e397f28351b9f4a5
TrpFrog <dev@trpfrog.net>
2022-02-19 03:21:26 +0900

    Refactor crawler.py

--- crawler.py
+++ crawler.py
@@ -9,5 +9,13 @@ crawl_image_keywords = [
 
 for keywords in crawl_image_keywords:
     for keyword in keywords:
-        crawler = BingImageCrawler(storage={"root_dir": f"img/{keywords[0]}"},downloader_threads=5)
-        crawler.crawl(keyword=keyword, max_num=100, file_idx_offset='auto')
+        dir_to_save_img = f"img/{keywords[0]}"
+        crawler = BingImageCrawler(
+            storage={"root_dir": dir_to_save_img},
+            downloader_threads=5
+        )
+        crawler.crawl(
+            keyword=keyword,
+            max_num=100,
+            file_idx_offset='auto'
+        )
