f7160e3f2c51a0b71f326aeb4eea564f8a8e66ff
TrpFrog <dev@trpfrog.net>
2022-02-19 09:37:22 +0900

    Make good_resize better

--- src/good_resize.m
+++ src/good_resize.m
@@ -5,8 +5,20 @@ function img = good_resize(img)
     [H, W, ~] = size(img);
 
     goal = 320 * 240;
-    h = round(sqrt(H / W * goal));
-    w = round(sqrt(W / H * goal));
+    h = sqrt(H / W * goal);
+    w = sqrt(W / H * goal);
+
+    hs = [floor(h) floor(h) ceil(h) ceil(h)];
+    ws = [floor(w) ceil(w) floor(w) ceil(w)];
+
+    h = 0;
+    w = 0;
+    for i = 1:4
+        if abs(h * w - goal) > abs(hs(i) * ws(i) - goal)
+            h = hs(i);
+            w = ws(i);
+        end
+    end
     
     img = imresize(img, [h, w]);
 end
