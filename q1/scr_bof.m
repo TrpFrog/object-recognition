54a9133082b80e80c745c416230cf8b6637065d5
TrpFrog <dev@trpfrog.net>
2022-02-21 00:13:16 +0900

    Add support for print_top3_imgs to scr_bof

--- q1/scr_bof.m
+++ q1/scr_bof.m
@@ -1,20 +1,25 @@
+trp_paths  = get_img_fnames('trumpet');
+trb_paths  = get_img_fnames('trombone');
+frog_paths = get_img_fnames('frog');
+leek_paths = get_img_fnames('leek');
+
 disp('Generating BoF matrix of trumpets and trombones...');
 load('codebook_tptb.mat');
-trp  = bof_matrix(get_img_fnames('trumpet'), codebook);
-trb  = bof_matrix(get_img_fnames('trombone'),codebook);
+trp_mat = bof_matrix(trp_paths, codebook);
+trb_mat = bof_matrix(trb_paths, codebook);
 
 disp('Generating BoF matrix of frogs and leeks...');
 load('codebook_frle.mat');
-frog = bof_matrix(get_img_fnames('frog'), codebook);
-leek = bof_matrix(get_img_fnames('leek'), codebook);
+frog_mat = bof_matrix(frog_paths, codebook);
+leek_mat = bof_matrix(leek_paths, codebook);
 
 label = [-ones(100, 1); ones(100, 1)];
 
 disp('[Trumpets and Trombones]');
-five_fold_cross_validation([trp; trb], label, @f_learn, @f_test);
+five_fold_cross_validation([trp_mat; trb_mat], label, [trp_paths, trb_paths], @f_learn, @f_test);
 
 disp('[Frogs and Leeks]')
-five_fold_cross_validation([frog; leek], label, @f_learn, @f_test);
+five_fold_cross_validation([frog_mat; leek_mat], label, [frog_paths, leek_paths], @f_learn, @f_test);
 
 % モデル学習用関数
 function model = f_learn(train_data, train_label)
@@ -22,15 +27,15 @@ function model = f_learn(train_data, train_label)
 end
 
 % テスト用関数
-function ac = f_test(model, eval_data, eval_label)
+function [ac, scores, is_correct] = f_test(model, eval_data, eval_label)
     n = size(eval_data, 1);
-    [plabel, ~] = predict(model, eval_data);
+    [plabel, scores] = predict(model, eval_data);
 
     % label は 1 か -1 かのどちらかである。
     % もし分類が正しければ plabel と eval_label はそれぞれ 1 と 1, または -1 と -1 になるはずであり、
     % 誤りならば 1 と -1 となるはずである。
     % すなわち2つのベクトルの和の各要素は、正解ならば 2 か -2、不正解ならば 0 となる。
     % よって「(plabel + eval_label) / 2 の絶対値の和」が「正しく分類された数」となる。
-    ac = sum(abs(plabel + eval_label)) / 2;
-    ac = ac / n;
+    is_correct = abs(plabel + eval_label) / 2;
+    ac = sum(is_correct) / n;
 end
