2380e5a7fd9976c32092e467e3e4374347431966
TrpFrog <dev@trpfrog.net>
2022-02-21 00:12:43 +0900

    Add support for print_top3_imgs to scr_colorhis

--- q1/scr_colorhis.m
+++ q1/scr_colorhis.m
@@ -1,15 +1,20 @@
-trp  = colorhis_matrix(get_img_fnames('trumpet'));
-trb  = colorhis_matrix(get_img_fnames('trombone'));
-frog = colorhis_matrix(get_img_fnames('frog'));
-leek = colorhis_matrix(get_img_fnames('leek'));
+trp_paths  = get_img_fnames('trumpet');
+trb_paths  = get_img_fnames('trombone');
+frog_paths = get_img_fnames('frog');
+leek_paths = get_img_fnames('leek');
+
+trp_mat = colorhis_matrix(trp_paths);
+trb_mat = colorhis_matrix(trb_paths);
+frog_mat = colorhis_matrix(frog_paths);
+leek_mat = colorhis_matrix(leek_paths);
 
 label = [zeros(100, 1); ones(100, 1)];
 
 disp('[Trumpets and Trombones]');
-five_fold_cross_validation([trp; trb], label, @f_learn, @f_test);
+five_fold_cross_validation([trp_mat; trb_mat], label, [trp_paths, trb_paths], @f_learn, @f_test);
 
 disp('[Frogs and Leeks]')
-five_fold_cross_validation([frog; leek], label, @f_learn, @f_test);
+five_fold_cross_validation([frog_mat; leek_mat], label, [frog_paths, leek_paths], @f_learn, @f_test);
 
 % モデル学習用関数
 function model = f_learn(train_data, train_label)
@@ -19,19 +24,23 @@ function model = f_learn(train_data, train_label)
 end
 
 % テスト用関数
-function ac = f_test(model, eval_data, eval_label)
+function [ac, scores, is_correct] = f_test(model, eval_data, eval_label)
     train_data = model{1};
     train_label = model{2};
+
+    scores = zeros(length(eval_label), 1);
+    is_correct = false(length(eval_label), 1);
 
     n = size(eval_data, 1);
     ac = 0;
     for i = 1:n
         % 学習データの中でも最も近いベクトルのindexをもらう
-        idx = nearest_vec(train_data, eval_data(i, :));
-
+        [scores(i), idx] = nearest_vec(train_data, eval_data(i, :));
+        
         % ラベルが同じならば正解
         if train_label(idx) == eval_label(i)
             ac = ac + 1;
+            is_correct(i) = true;
         end
     end
 
