a4f8e152428457ed595edc4a37eed9de0f57f7f4
TrpFrog <dev@trpfrog.net>
2022-02-20 23:50:22 +0900

    Fix bof_matrix does not work

--- q1/bof_matrix.m
+++ q1/bof_matrix.m
@@ -1,6 +1,7 @@
 function bof = bof_matrix(paths, codebook)
 %BOF_MATRIX 画像のパスが含まれたセル配列を受け取り、各行がBoFベクトルになっている行列を返す
     n = length(paths);
+    
     bof = zeros(n, 1000);
     for i = 1:n
         disp(strcat(string(i), '/', string(n), ': ', paths{i}));
@@ -10,7 +11,7 @@ function bof = bof_matrix(paths, codebook)
         p = createRandomPoints(I,3000);
         [f,p2] = extractFeatures(I,p);
         for j = 1:size(p2, 1)
-            res = nearest_vec(codebook, f(j, :));
+            [~, res] = nearest_vec(codebook, f(j, :));
             bof(i, res) = bof(i, res) + 1; 
         end
     end
