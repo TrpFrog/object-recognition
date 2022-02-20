a14434366236f1b7b8d5215c3850c888c6711595
TrpFrog <dev@trpfrog.net>
2022-02-20 23:45:47 +0900

    Make return distance in the function nearest_vec

--- q1/nearest_vec.m
+++ q1/nearest_vec.m
@@ -1,6 +1,6 @@
-function idx = nearest_vec(mat, vec)
+function [dist, idx] = nearest_vec(mat, vec)
 %NEAREST_VEC 行列 mat の行ベクトルの中で vec と最も近いもののindexを返す
     n = size(mat, 1);
     mat = mat - repmat(vec, n, 1);
-    [~, idx] = min(sum(mat.^2, 2)); % a^2 <= b^2 ならば |a| <= |b|
+    [dist, idx] = min(sum(mat.^2, 2)); % a^2 <= b^2 ならば |a| <= |b|
 end
