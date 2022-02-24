52d7d18889981228cc6a1abc069c1e052fc9230c
TrpFrog <dev@trpfrog.net>
2022-02-24 20:06:38 +0900

    Update dcnn_matrix.m

--- q1/dcnn_matrix.m
+++ q1/dcnn_matrix.m
@@ -7,8 +7,14 @@ function mat = dcnn_matrix(paths, net, layer, dcnn_size)
     mat = zeros(length(paths), dcnn_size);
     for i = 1:length(paths)
         waitbar(i/length(paths),f,strcat('Loading, ', paths{i}, '...'));
+        
         I = imread(paths{i});
         I = good_resize(I);
+        if size(I, 3) < 3
+            % グレースケール画像の場合は次元を拡張する
+            % 参考: https://jp.mathworks.com/help/images/convert-between-image-types.html
+            I = cat(3, I, I ,I);
+        end
         reimg = imresize(I, net.Layers(1).InputSize(1:2));
         dcnnf = activations(net,reimg,layer);
         dcnnf = squeeze(dcnnf);
